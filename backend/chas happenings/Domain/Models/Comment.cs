2c1a20486747a86fab9506808ab320b405558ea6
samihoy <maximillian.lundberg@chasacademy.se>
2025-10-12 21:41:55 +0200

    added model for  comments

---
+++ backend/chas happenings/Domain/Models/Comment.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+        public User Author { get; set; }
+        public string Text { get; set; }
+        public string ProfilePicture { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public Event Event { get; set; }
+    }
+}
