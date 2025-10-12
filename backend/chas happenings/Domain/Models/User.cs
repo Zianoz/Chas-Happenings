b88281c9934ce34199b64ed1ac51531fed266a7b
samihoy <maximillian.lundberg@chasacademy.se>
2025-10-12 21:42:45 +0200

    added model for  users

---
+++ backend/chas happenings/Domain/Models/User.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Domain.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string PasswordHash { get; set; }
+        public string Course { get; set; }
+        public string Role { get; set; }
+        public string ProfilePictureUrl { get; set; }
+        public string UserDescription { get; set; }
+
+        public ICollection<Event> CreatedEvents { get; set; } = new List<Event>();
+        //public ICollection<UserEventInterested> Interested { get; set; } = new List<UserEventInterested>();
+        //public ICollection<UserEventAttending> Attending { get; set; } = new List<UserEventAttending>();
+        public ICollection<Comment> Comments { get; set; } = new List<Comment>();
+    }
+}
