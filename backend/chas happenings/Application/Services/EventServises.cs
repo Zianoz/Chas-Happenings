270111a489d7df13194daf14ac71b1176ec4c9f8
samihoy <maximillian.lundberg@chasacademy.se>
2025-10-15 18:37:32 +0200

    added but not implemented Services for Events

---
+++ backend/chas happenings/Application/Services/EventServises.cs
@@ -0,0 +1,19 @@
+﻿using Application.Interfaces.Irepositories;
+using Application.Interfaces.IServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Services
+{
+    internal class EventServises : IEventServices
+    {
+        private readonly IEventRepositories _repo;
+        public EventServises(IEventRepositories repo)
+        {
+            _repo = repo;
+        }
+    }
+}
