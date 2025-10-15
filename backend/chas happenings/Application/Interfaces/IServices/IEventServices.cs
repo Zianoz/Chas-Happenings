a06fcb95f36e8f850eb0aad7154f7557bccdf5e9
samihoy <maximillian.lundberg@chasacademy.se>
2025-10-15 18:37:03 +0200

    added interfaces for event services

---
+++ backend/chas happenings/Application/Interfaces/IServices/IEventServices.cs
@@ -0,0 +1,22 @@
+﻿using Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Interfaces.IServices
+{
+    public interface IEventServices
+    {
+        Task<Event?> GetEventByIdServicesAsync(int EventId);
+        Task<int> DeleteEventsByIdServicesAsync(int EventId);
+        Task<int> AddEventServicesAsync(Event Event);
+        Task<int> UpdateEventServicesAsync(Event Event);
+
+        Task<List<Event?>> GetEventsByDateTimeServicesAsync(DateTime startdate, DateTime endDate);
+        Task<List<Event?>> GetEventsByCategoriesAndDateServicesAsync(HashSet<string> EventTags, DateTime startdate, DateTime endDate);
+        Task<List<Event?>> GetEventsByTypeAndDateServicesAsync(HashSet<string> EventType, DateTime startdate, DateTime endDate);
+
+    }
+}
