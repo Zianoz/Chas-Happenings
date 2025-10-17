1a6150935dfb574d5ca483aeba7b06f380aacef0
samihoy <maximillian.lundberg@chasacademy.se>
2025-10-17 05:18:45 +0200

    Created a User DTO for getting name, picture and ID

---
+++ backend/chas happenings/Application/DTOs/UserDTOs/GetUserNamePictureDTO.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DTOs.UserDTOs
+{
+    internal class GetUserNamePictureDTO
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string ProfilePictureUrl { get; set; }
+
+    }
+}
