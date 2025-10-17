b10417347d70ff09b8e3aa23faac80268a1f0664
samihoy <maximillian.lundberg@chasacademy.se>
2025-10-17 05:17:09 +0200

    created a tag DTO to get only the tag name

---
+++ backend/chas happenings/Application/DTOs/TagDTOs/GetTagNameDTO.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DTOs.TagDTOs
+{
+    internal class GetTagNameDTO
+    {
+        public string TagName { get; set; }
+    }
+}
