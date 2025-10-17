7832bc9d3a243d631a0016f494933307638a3fd7
Zian <zianpakapol@gmail.com>
2025-10-17 14:51:03 +0200

    working on Crud for Comments

---
+++ backend/chas happenings/Infrastructure/Repositories/CommentRepository.cs
@@ -0,0 +1,21 @@
+﻿using Application.Interfaces.Irepositories;
+using Application.Interfaces.IServices;
+using Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repositories
+{
+    public class CommentRepository : ICommentRepository
+    {
+        private readonly ChasHappeningsDbContext _context;
+
+        public CommentRepository(ChasHappeningsDbContext context)
+        {
+            _context = context;
+        }
+    }
+}
---
+++ backend/chas happenings/web/Controllers/CommentController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Application.Interfaces.IServices;
+
+namespace chas_happenings.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CommentController : ControllerBase
+    {
+        private readonly ICommentService _commentService;
+
+        public CommentController(ICommentService commentService)
+        {
+            _commentService = commentService;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<int>> AddComment(string commentText)
+        {
+
+
+            return Ok("Comment added successfully");
+        }
+    }
+}
