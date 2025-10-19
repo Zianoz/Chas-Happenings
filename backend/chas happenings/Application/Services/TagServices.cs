b79c6decbaa54f8cb691811631de5340ce4f4f35
lucine1029 <sherry.jing@hotmail.com>
2025-10-19 11:11:32 +0200

    implemented  Service methods for Tag

---
+++ backend/chas happenings/Application/Services/TagServices.cs
@@ -0,0 +1,49 @@
+﻿using Application.DTOs.TagDTOs;
+using Application.Interfaces.Irepositories;
+using Application.Interfaces.IServices;
+using Application.Mappers.DTOMappers;
+using Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Services
+{
+    internal class TagServices : ITagServices
+    {
+        private readonly ITagRepositories _repo;
+        public TagServices(ITagRepositories repo)
+        {
+            _repo = repo;
+        }
+
+
+        public Task<int> AddTagServiceAsync(CreateTagDTO tagDTO)
+        {
+            var newTag = DTOTagMapper.CreateTagModelFromDTOs(tagDTO);
+
+        }
+
+        public Task<int> DeleteTagByIdServiceAsync(int TagId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Tag?>> GetAllTagsServiceAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Tag?> GetTagByIdServiceAsync(int TagId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> UpdateTagServiceAsync(Tag tag)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
