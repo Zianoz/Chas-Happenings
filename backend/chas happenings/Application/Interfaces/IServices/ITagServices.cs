ab2ac4de01539789dc788c31d51ab06fdd76a995
lucine1029 <sherry.jing@hotmail.com>
2025-10-19 11:08:21 +0200

    Create interface for Tag: ITagServices, ITagRepositories

---
+++ backend/chas happenings/Application/Interfaces/IServices/ITagServices.cs
@@ -0,0 +1,21 @@
+﻿using Application.DTOs.TagDTOs;
+using Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Interfaces.IServices
+{
+    internal interface ITagServices
+    {
+        // Standard CRUD Operationer
+        Task<Tag?> GetTagByIdServiceAsync(int TagId);
+        Task<int> DeleteTagByIdServiceAsync(int TagId);
+        Task<int> AddTagServiceAsync(CreateTagDTO tagDTO);
+        Task<int> UpdateTagServiceAsync(Tag tag);
+        // Admin Endpoints 
+        Task<List<Tag?>> GetAllTagsServiceAsync();
+    }
+}
