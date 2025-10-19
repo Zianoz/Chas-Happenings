115e5e8a6dc8a12dd1c725eed8addf46d72c518f
lucine1029 <sherry.jing@hotmail.com>
2025-10-19 11:07:14 +0200

    Made a DTO mapper for creating Tag and updating Tag

---
+++ backend/chas happenings/Application/Mappers/DTOMappers/DTOTagMapper.cs
@@ -0,0 +1,31 @@
+﻿using Application.DTOs.TagDTOs;
+using Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Mappers.DTOMappers
+{
+    internal class DTOTagMapper
+    {
+        public static Tag CreateTagModelFromDTOs(CreateTagDTO tagDTO)
+        {
+            return new Tag
+            {
+                TagName = tagDTO.TagName,
+            };
+        }
+
+        public static Tag UpdateTagModelFromDTOs(UpdateTagDTO tagDTO, int tagId)
+        {
+            return new Tag
+            {
+                Id = tagId,
+                TagName = tagDTO.TagName,
+            };
+        }
+
+    }
+}
