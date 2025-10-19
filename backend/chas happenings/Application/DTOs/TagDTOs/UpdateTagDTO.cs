27ab49fba52d8426d4b60d2036702fac8694832e
lucine1029 <sherry.jing@hotmail.com>
2025-10-19 11:05:06 +0200

    Create a CreateTagDTO and UpdateTagDTO for Tag name

---
+++ backend/chas happenings/Application/DTOs/TagDTOs/UpdateTagDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DTOs.TagDTOs
+{
+    internal class UpdateTagDTO
+    {
+        public string TagName { get; set; }
+    }
+}
