26472943581cb964170b790bc189c54017cf8898
lucine1029 <sherry.jing@hotmail.com>
2025-10-24 16:10:03 +0200

    Setup the user-crud branch

---
+++ backend/chas happenings/Application/Interfaces/IServices/IUserService.cs
@@ -0,0 +1,28 @@
+﻿using Application.DTOs.EvenDTOs;
+using Application.DTOs.UserDTOs;
+using Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Interfaces.IServices
+{
+    public interface IUserService
+    {
+        //stand CRUD Operations
+        Task<User?> GetUserByIdServicesAsync(int userId); //Jing
+        Task<bool> DeleteUserByIdServicesAsync(int userId);//Jing
+        Task<int> AddUserServicesAsync(CreateUserDTO userDTO);//Zian
+        Task<bool> UpdateUserServicesAsync(int userId, UpdateUserDTO updateUserDTO);//Zian
+
+        //extra user operations
+        Task<GetUserAllDataDTO?> GetUserAllDataAsync(int userId);// includes events created, comments, attending, interested
+        Task<GetUserCommentsDTO?> GetUserCommentsAsync(int userId);
+
+        // for admin
+        Task<List<User?>> GetAllUsers();//Jing
+        Task<List<User?>> GetListOfUsers(List<int>userIds);//Zian
+    }
+}
