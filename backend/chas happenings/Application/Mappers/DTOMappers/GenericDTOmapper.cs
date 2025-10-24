7e6e0dd4ed65ffa8ac9772d6266d8ddd3b8397ff
samihoy <maximillian.lundberg@chasacademy.se>
2025-10-24 07:05:29 +0200

    Added a generic DTO mapper

---
+++ backend/chas happenings/Application/Mappers/DTOMappers/GenericDTOmapper.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Mappers.DTOMappers
+{
+    internal class GenericDTOmapper
+    {
+        public static void Mapper<Tmodel, Tdto>(Tmodel model, Tdto dto) where Tmodel : class where Tdto : class
+        {
+            var modelFeildsList = typeof(Tmodel).GetProperties();
+            var dtoFeildsList = typeof(Tdto).GetProperties();
+
+            foreach (var feildProperty in modelFeildsList)
+            {
+                var modelPropertyValue = feildProperty.GetValue(model);
+
+                if (modelPropertyValue == null)
+                {
+                    continue;
+                }
+
+                var dtoFeildproperty = dtoFeildsList.FirstOrDefault(f => f.Name == feildProperty.Name);
+
+                if (dtoFeildproperty == null)
+                {
+                    continue;
+                }
+
+                if (dtoFeildproperty.PropertyType.IsInstanceOfType(modelPropertyValue))
+                {
+                    dtoFeildproperty.SetValue(dto, modelPropertyValue);
+                }
+            }
+        }
+    }
+}
