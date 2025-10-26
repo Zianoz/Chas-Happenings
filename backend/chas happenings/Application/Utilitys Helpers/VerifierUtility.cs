3e775f22fa281701d252ee58f55f09782fea7989
samihoy <maximillian.lundberg@chasacademy.se>
2025-10-26 09:19:13 +0100

    created a varifyer Helper class to handle verification of repo operations

---
+++ backend/chas happenings/Application/Utilitys Helpers/VerifierUtility.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Identity.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Utilitys_Helpers
+{
+    internal class VerifierUtility
+    {
+        public static bool VerifyResponse(int response)
+        {
+            if(response>0)
+            {
+                return true;
+            }
+            return false;
+        }
+        public static T VerifyObject<T>(T? databaseObject) where T: class
+        {
+            if(databaseObject==null)
+            {
+                throw new InvalidDataException($"Database operation failed, repo returned: {databaseObject} ");
+            }
+            return databaseObject;
+        }
+    }
+}
