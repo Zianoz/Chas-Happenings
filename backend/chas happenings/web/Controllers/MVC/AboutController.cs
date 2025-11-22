4981def1f52545a4dae009bcdb854d05af2d873e
lucine1029 <sherry.jing@hotmail.com>
2025-11-22 16:19:39 +0100

    Add Seo meta class and configs, and add About page

---
+++ backend/chas happenings/web/Controllers/MVC/AboutController.cs
@@ -0,0 +1,26 @@
+﻿using chas_happenings.Models.Seo;
+using Microsoft.AspNetCore.Mvc;
+
+namespace chas_happenings.Controllers.MVC
+{
+    public class AboutController : Controller
+    {
+
+        public IActionResult About()
+        {
+            SeoConfig.Set(ViewBag, new SeoMeta
+            {
+                Title = "About Us - My Website",
+                Description = "Learn more about our team and mission.",
+                Keywords = "about, company, team",
+
+                CanonicalUrl = Url.Action("About", "About", null, Request.Scheme),
+
+                OgImage = Url.Content("~/images/og/about.jpg")
+            });
+
+            return View();
+        }
+
+    }
+}
